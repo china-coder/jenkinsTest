a35b221dfe283d2b55356945575ebb388340a07d
abcd420840 <851777749@qq.com>
2018-01-30 17:05:30 +0800

    add test

---
+++ src/test/java/MainTest.java
@@ -0,0 +1,16 @@
+import org.junit.Test;
+
+/**
+ * @Project:jenkinsTest
+ * @Package Name:PACKAGE_NAME
+ * @Description:TODO
+ * @Author:chenjun
+ * @Creation Date:2018年01月30日
+ * ModificationHistory    who      when    what
+ */
+public class MainTest {
+    @Test
+    public void testOne(){
+        System.out.println("haha");
+    }
+}
