dd5d9d4f565401d89d740d4513ffbfecb00ba88a
abcd420840 <851777749@qq.com>
2018-01-31 10:26:23 +0800

    add 修改测试位置

---
+++ src/main/java/CoverageTest.java
@@ -0,0 +1,23 @@
+
+public class CoverageTest {
+
+    public CoverageTest() {
+        // TODO Auto-generated constructor stub
+    }
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+    
+    public static int  testadd(int x, int y){
+        System.out.println("haha2");
+        int c = 0;
+        if(x == 10){
+            c = x + y;
+        }else{
+            c = (x + y)*2;
+        }
+        return c;
+    }
+}
